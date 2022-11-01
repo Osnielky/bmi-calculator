fa2b3ada35e70b1e86135e6bad43f29f01adf652
Osnielky <osnielkisrq@gmail.com>
2022-11-01 13:58:05 -0400

    from mac

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
 
+import {Box, TextField } from '@mui/material'
+
 function App() {
   return (
     <div className="App">
+    <h1>Template is running</h1>
     <h1>Template is running</h1>
     </div>
   );
