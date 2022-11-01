5681bbae643f69cf3fe531915d345530625b8e4c
Osnielky <osnielkisrq@gmail.com>
2022-11-01 17:08:30 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 
-
+import {TextField , Button} from '@mui/material'
 
 function App() {
   return (
     <div className="App">
     <h1>Template is running</h1>
     <h1>Template is running</h1>
+    <Button variant="outlined">Testing</Button>
+
     </div>
   );
 }
