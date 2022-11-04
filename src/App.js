a4bbc38b6003c13c4284edc72104cfbe331c2a73
Osnielky <osnielkisrq@gmail.com>
2022-11-04 11:26:19 -0400

    completting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
 import './App.css';
 
-import {TextField , Button} from '@mui/material'
+import {Radio,FormControlLabel, RadioGroup,FormLabel,FormControl,Box, TextField , Button} from '@mui/material'
 
 function App() {
+  
+  
+  
   return (
     <div className="App">
-    <h1>Template is running</h1>
-    <h1>Template is running</h1>
+
+<Box >
+
+<FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <FormControlLabel value="male" control={<Radio />} label="Male" />
+    <FormControlLabel value="other" control={<Radio />} label="Other" />
+  </RadioGroup>
+</FormControl>
+
+</Box>
+
+
+    <h1>Running BMI Calculator</h1>
+
     <Button variant="outlined">Testing</Button>
 
     </div>
